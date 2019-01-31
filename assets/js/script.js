21690c036d013d7aa456d880a75375c62c728789
cappydev <kevin.lanel@wanadoo.fr>
2019-01-31 13:40:06 +0100

    corrigé exo2

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function show(){
+  document.getElementById('text').style.display="visible";
+}
+
+function hide(){
+  document.getElementById('text').style.display="hidden";
+}
+
+document.getElementById('text').style.display="hidden";
